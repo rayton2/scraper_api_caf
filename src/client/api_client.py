3c2e64159ae1a84c8e9a059dd2c18a2bd758eb5a
rayton2 <vitorkerrigan@live.com>
2025-08-05 23:22:57 -0300

    Testes

--- src/client/api_client.py
+++ src/client/api_client.py
@@ -26,35 +26,12 @@ class APIClient:
         return response
 
     def get_consulta_publica(self, uf, codigo_municipio, pagina=1, tamanho_pagina=9999):
-        """
-        Fetches public consultation data for a specific municipality.
-
-        Args:
-            uf (str): The UF code.
-            codigo_municipio (str): The municipality code.
-            pagina (int): The page number for pagination.
-            tamanho_pagina (int): The number of results per page.
-
-        Returns:
-            dict: JSON response containing public consultation data.
-        """
         endpoint = f"{self.base_url}/api/ufpa/consulta-publica?uf={uf}&codigoMunicipio={codigo_municipio}&pagina={pagina}&tamanhoPagina={tamanho_pagina}"
         response = self._get_request(endpoint)
         return response
 
     def _get_request(self, url):
-        """
-        Makes a GET request to the specified URL.
 
-        Args:
-            url (str): The URL to send the GET request to.
-
-        Returns:
-            dict: JSON response from the API.
-
-        Raises:
-            Exception: If the request fails or returns an error.
-        """
         response = requests.get(url, headers=self.headers)
         if response.status_code != 200:
             raise Exception(f"Error fetching data: {response.status_code} - {response.text}")
